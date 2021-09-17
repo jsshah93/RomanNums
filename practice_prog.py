2a64c996dab603ffc4ce3f51acc9f5d886e88c40
jsshah93 <67244782+jsshah93@users.noreply.github.com>
2021-09-17 14:04:30 +0100

    Update practice_prog.py

--- practice_prog.py
+++ practice_prog.py
@@ -1,15 +1,15 @@
 import practice_mod
 
-# below code changes number to roman numerals.
+# Hash out one program to run and vice versa.
+
+# Below code changes number to roman numerals.
 num = int(input("Type a number to convert to Roman Numerals:  "))
 abc = practice_mod.IntToRoman()
-
 retVal = abc.calc(num) 
-
 print(retVal)
 
 
-# below code changes roman numerals to numbers
+# Below code changes roman numerals to numbers.
 xyz = practice_mod.RomanToInt()
 retVal = xyz.calc2("MCMXCIX")
 print(retVal)
